e8c0b73445a457d031ed4c39197700abda1319c6
Vivek Sharma <115217567+VivekSharma1103@users.noreply.github.com>
2024-09-23 11:36:18 +0530

    Delete Assignment-1 directory

--- Assignment-1/q3.js
+++
@@ -1,4 +0,0 @@
-// Using a for loop to print numbers from 1 to 10
-for (let i = 1; i <= 10; i++) {
-    console.log(i);
-}
--- Assignment-1/q4.js
+++
@@ -1,7 +0,0 @@
-// Function to greet the user by name
-function greet(name) {
-    console.log("Hello, " + name + "!");
-}
-
-// Example usage:
-greet("Alice");
