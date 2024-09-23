13943e30dc1db5c6f72e71fcd0cc2572e7498fae
Vivek Sharma <115217567+VivekSharma1103@users.noreply.github.com>
2024-09-23 11:36:42 +0530

    Delete Assignment-3 directory

--- Assignment-3/q1.js
+++
@@ -1,8 +0,0 @@
-function delayedMessage(message, delay) {
-    setTimeout(() => {
-        console.log(message);
-    }, delay);
-}
-
-// Example usage
-delayedMessage("Hello after 2 seconds!", 2000);
